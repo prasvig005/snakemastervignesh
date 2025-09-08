9678fb767ea529d3241b7e9323f45e35c65fbb73
prasvig005 <prasvig005@gmail.com>
2025-09-08 20:42:19 +0530

    Add files via upload

---
+++ service-worker[1].js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'fsg-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request).then(resp => {
+      return caches.open(CACHE_NAME).then(cache => {
+        cache.put(event.request, resp.clone());
+        return resp;
+      });
+    }))
+  );
+});
